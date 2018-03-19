61e3838acffaf39c10edc3264b24d0baa7f4e19b
Manuel Gomez <gomez@rockhopper-01.cs.wisc.edu>
2018-03-19 11:29:15 -0500

    2nd commit: Working on getters

--- Student.cpp
+++ Student.cpp
@@ -2,12 +2,8 @@
 #include "Student.hpp"
 
 int Student::numStudents = 0; //The number of references of this class.
+static int ID = 0;	      //The monotonically increasing ID per student.
 
-/**
- * @brief Constructor for Student class.
- * 
- * @param 
- */
 Student::Student (std::string name, int yearOfBirth,
  const std::vector<double> &assignmentsScore,
  double projectScore) {
@@ -15,13 +11,54 @@ Student::Student (std::string name, int yearOfBirth,
 	Student::yearOfBirth = yearOfBirth;           //The year of birth of student
 	Student::assignmentsScore = assignmentsScore; //Vect of student's assignment scores.
 	Student::projectScore = projectScore;	      //Vect of their project scores.
-	
-	
+	++Student::numStudents;			      //Increment num references made.	
 }
 
-/**
- *
- *
- *
- *
- */
+int getID() {
+	++ID;	//Increment ID; lowest ID is 1.
+	return ID;
+}
+
+std::string Student::getName() {
+	return name;
+}
+
+int Student::getYearOfBirth() {
+	return yearOfBirth;
+}
+
+int Student::getAge() {
+	return 2016 - getYearOfBirth(); //Current year - YOB = age.
+} 
+
+//FIXME how to return properly?
+const std::vector<double>& Student::getAssignmentsScore() {
+	return assignmentsScore;
+}
+
+double Student::getProjectScore() {
+	return projectScore;
+}
+
+int Student::getNumStudents() {
+	return numStudents;
+}
+//Implement the virtual method printDetails() which should print the id, name, age, assignments score, project score, total score, and grade for a given student
+
+//void printDetail
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
